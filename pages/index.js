6b2f6c8e5f1fd099e74a094762a9420c950e32d5
Orry Mevorach <orry.mevorach@gmail.com>
2022-08-31 12:13:38 -0400

    fix mobile styles, add icon

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
+import Head from 'next/head';
 import globalStyles from '@styles/global.module.scss';
-// import '@styles/fonts.scss';
 import RhcpLogo from '@components/rhco-logo/rhcp-logo';
 import Rankings from '@components/rankings/rankings';
 import SongCount from '@components/song-count/song-count';
@@ -8,12 +8,22 @@ import useWindowSize from '../hooks/useWindowSize';
 export default function Home() {
   const { isDesktop } = useWindowSize();
   return (
-    <main className={globalStyles.body}>
-      <RhcpLogo />
-      <div className={`${isDesktop && globalStyles.row}`}>
-        <Rankings />
-        <SongCount />
-      </div>
-    </main>
+    <div>
+      <Head>
+        <title>RHCP Mock Draft</title>
+        <link
+          rel="icon"
+          href="https://m.media-amazon.com/images/I/41VvATDmI3L._AC_SY450_.jpg"
+        />
+      </Head>
+      ;
+      <main className={globalStyles.body}>
+        <RhcpLogo />
+        <div className={`${isDesktop && globalStyles.row}`}>
+          <Rankings />
+          <SongCount />
+        </div>
+      </main>
+    </div>
   );
 }
